86d33da434b9b39142c87a9effdb28c5fc5359db
user <user@user-PC>
2017-01-17 18:37:22 +0530

    ravi

--- visog-job-portal-ui/WebContent/index.jsp
+++ visog-job-portal-ui/WebContent/index.jsp
@@ -11,20 +11,26 @@
  
  <h4><a href="/visog-job-portal-ui/hello">Click Here</a></h4>
   <h4><a href="/visog-job-portal-ui/reg">UsersRegistration</a></h4>
-    <h4><a href="/visog-job-portal-ui/courses">Courses</a></h4>
-      <h4><a href="/visog-job-portal-ui/domains">Domains</a></h4>
-  <h4><a href="/visog-job-portal-ui/gender">Gender</a></h4>
-  <h4><a href="/visog-job-portal-ui/country">Country</a></h4>
-  <h4><a href="/visog-job-portal-ui/state">State</a></h4>
+  <h4><a href="/visog-job-portal-ui/roles">Roles</a></h4>
+   <h4><a href="/visog-job-portal-ui/gender">Gender</a></h4>
+   <h4><a href="/visog-job-portal-ui/country">Country</a></h4>
+    <h4><a href="/visog-job-portal-ui/state">State</a></h4>
   <h4><a href="/visog-job-portal-ui/city">City</a></h4>
-  <h4><a href="/visog-job-portal-ui/employertype">EmployerType</a></h4>
-   <h4><a href="/visog-job-portal-ui/employmenttype">EmploymentType</a></h4>
-   <h4><a href="/visog-job-portal-ui/currency">Currency</a></h4>
-    <h4><a href="/visog-job-portal-ui/roles">Roles</a></h4>
-     <h4><a href="/visog-job-portal-ui/educationtype">Education Type</a></h4>
-  <h4><a href="/visog-job-portal-ui/filetype">File Type</a></h4>
-  <h4><a href="/visog-job-portal-ui/university">University</a></h4>
+   <h4><a href="/visog-job-portal-ui/addresstype">Address Type</a></h4>
+    <h4><a href="/visog-job-portal-ui/courses">Courses</a></h4>
     <h4><a href="/visog-job-portal-ui/specilization">Specilization</a></h4>
+    <h4><a href="/visog-job-portal-ui/university">University</a></h4>
+      <h4><a href="/visog-job-portal-ui/educationtype">Education Type</a></h4>
+      <h4><a href="/visog-job-portal-ui/languages">Languages </a></h4>
+       <h4><a href="/visog-job-portal-ui/currency">Currency</a></h4>
+        <h4><a href="/visog-job-portal-ui/industry">Industry</a></h4>
+      <h4><a href="/visog-job-portal-ui/domains">Domains</a></h4>
+       <h4><a href="/visog-job-portal-ui/jobrole">Job Role</a></h4>
+ 		 <h4><a href="/visog-job-portal-ui/status">Status</a></h4>
+  <h4><a href="/visog-job-portal-ui/employmenttype">EmploymentType</a></h4>
+   <h4><a href="/visog-job-portal-ui/employertype">EmployerType</a></h4>
+         <h4><a href="/visog-job-portal-ui/filetype">File Type</a></h4>
+  
     <h4><a href="/visog-job-portal-ui/index">index</a></h4>
  </center>
 </body>
--- visog-job-portal-ui/src/com/doj/spring4/controller/HelloSpring4Controller.java
+++ visog-job-portal-ui/src/com/doj/spring4/controller/HelloSpring4Controller.java
@@ -5,141 +5,147 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
-@RequestMapping("/")  
+@RequestMapping("/")
 public class HelloSpring4Controller {
-	
-	@RequestMapping("/user")  
-	public ModelAndView user() {  
+
+	@RequestMapping("/user")
+	public ModelAndView user() {
 		String message = "visog-job-portal-ui";
 		return new ModelAndView("main/user");
-	}  
-	
-	@RequestMapping("/userreg")  
-	public ModelAndView userreg() {  
+	}
+
+	@RequestMapping("/userreg")
+	public ModelAndView userreg() {
 		String message = "visog-job-portal-ui";
 		return new ModelAndView("users_Registration");
-	}  
-	
-	
-	@RequestMapping("/hello")  
-	public ModelAndView sayHello() {  
+	}
+
+	@RequestMapping("/hello")
+	public ModelAndView sayHello() {
 		String message = "visog-job-portal-ui";
 		return new ModelAndView("hello2");
-	}  
-	
-	@RequestMapping("/reg")  
-	public ModelAndView reg() {  
+	}
+
+	@RequestMapping("/reg")
+	public ModelAndView reg() {
 		String message = "visog-job-portal-ui";
 		return new ModelAndView("transaction/users_Registration");
-	}  
-	
-	@RequestMapping("/registration")  
-	public ModelAndView say() {  
+	}
+
+	@RequestMapping("/registration")
+	public ModelAndView say() {
 		String message = "visog-job-portal-ui";
 		return new ModelAndView("master/registration");
-	}  
-	@RequestMapping(value = "/gender", method = RequestMethod.GET)   
-	public String gender() {  
+	}
+
+	@RequestMapping(value = "/roles", method = RequestMethod.GET)
+	public String roles() {
+		String message = "visog-job-portal-ui";
+		return "master/roles";
+	}
+
+	@RequestMapping(value = "/gender", method = RequestMethod.GET)
+	public String gender() {
 		String message = "visog-job-portal-ui";
 		return "master/gender";
-	} 
-	
-	@RequestMapping(value = "/currency", method = RequestMethod.GET)   
-	public String currency() {  
-		String message = "visog-job-portal-ui";
-		return "master/currency";
-	} 
-	
-	@RequestMapping(value = "/industry", method = RequestMethod.GET)   
-	public String industry() {  
-		String message = "visog-job-portal-ui";
-		return "master/industry";
-	} 
-	
-	@RequestMapping(value = "/employmentType", method = RequestMethod.GET)   
-	public String empType() {  
-		String message = "visog-job-portal-ui";
-		return "master/employment_Type";
-	} 
-	
+	}
+
 	@RequestMapping(value = "/country", method = RequestMethod.GET)
-	public String country() {  
+	public String country() {
 		String message = "visog-job-portal-ui";
 
 		return "master/country";
-	} 
-	 
-	@RequestMapping(value = "/state", method = RequestMethod.GET)  
-	public String state() {  
+	}
+
+	@RequestMapping(value = "/state", method = RequestMethod.GET)
+	public String state() {
 		String message = "visog-job-portal-ui";
 		return "master/state";
-	} 
-	 
-	@RequestMapping(value= "/city" ,method = RequestMethod.GET)  
-	public String city() {  
+	}
+
+	@RequestMapping(value = "/city", method = RequestMethod.GET)
+	public String city() {
 		String message = "visog-job-portal-ui";
 		return "master/city";
-	} 
-	
-	@RequestMapping(value= "/courses" ,method = RequestMethod.GET)  
-	public String courses() {  
+	}
+
+	@RequestMapping(value = "/addresstype", method = RequestMethod.GET)
+	public String addresstype() {
+		String message = "visog-job-portal-ui";
+		return "master/address_Type";
+	}
+
+	@RequestMapping(value = "/courses", method = RequestMethod.GET)
+	public String courses() {
 		String message = "visog-job-portal-ui";
 		return "master/courses";
 	}
-	
 
-	@RequestMapping(value= "/domains" ,method = RequestMethod.GET)  
-	public String domains() {  
-		String message = "visog-job-portal-ui";
-		return "master/domains";
-	}
-	
-	@RequestMapping(value= "/employertype" ,method = RequestMethod.GET)  
-	public String employertype() {  
-		String message = "visog-job-portal-ui";
-		return "master/employer_Type";
-	}
-	@RequestMapping(value= "/university" ,method = RequestMethod.GET)  
-	public String university() {  
+	@RequestMapping(value = "/university", method = RequestMethod.GET)
+	public String university() {
 		String message = "visog-job-portal-ui";
 		return "master/university";
 	}
-	@RequestMapping(value= "/specilization" ,method = RequestMethod.GET)  
-	public String specilization() {  
+
+	@RequestMapping(value = "/educationtype", method = RequestMethod.GET)
+	public String educationtype() {
 		String message = "visog-job-portal-ui";
-		return "master/specialization";
+		return "master/education_Type";
 	}
-		@RequestMapping(value = "/roles", method = RequestMethod.GET)  
-		public String roles() {  
-			String message = "visog-job-portal-ui";
-			return "master/roles";
-		} 
-		 
-		@RequestMapping(value= "/filetype" ,method = RequestMethod.GET)  
-		public String filetype() {  
-			String message = "visog-job-portal-ui";
-			return "master/file_Type";
-		} 
-		
-		@RequestMapping(value= "/addresstype" ,method = RequestMethod.GET)  
-		public String addresstype() {  
-			String message = "visog-job-portal-ui";
-			return "master/address_Type";
-		}
-		
-		@RequestMapping(value= "/educationtype" ,method = RequestMethod.GET)  
-		public String educationtype() {  
-			String message = "visog-job-portal-ui";
-			return "master/education_Type";
-		}
-		
-		@RequestMapping(value= "/index" ,method = RequestMethod.GET)  
-		public String index() {  
-			String message = "visog-job-portal-ui";
-			return "main/index";
-		} 
-	
-	
+
+	@RequestMapping(value = "/currency", method = RequestMethod.GET)
+	public String currency() {
+		String message = "visog-job-portal-ui";
+		return "master/currency";
+	}
+
+	@RequestMapping(value = "/industry", method = RequestMethod.GET)
+	public String industry() {
+		String message = "visog-job-portal-ui";
+		return "master/industry";
+	}
+
+	@RequestMapping(value = "/domains", method = RequestMethod.GET)
+	public String domains() {
+		String message = "visog-job-portal-ui";
+		return "master/domains";
+	}
+
+	@RequestMapping(value = "/jobrole", method = RequestMethod.GET)
+	public String jobrole() {
+		String message = "visog-job-portal-ui";
+		return "master/job_Role";
+	}
+
+	@RequestMapping(value = "/status", method = RequestMethod.GET)
+	public String status() {
+		String message = "visog-job-portal-ui";
+		return "master/status";
+	}
+
+	@RequestMapping(value = "/employmentType", method = RequestMethod.GET)
+	public String empType() {
+		String message = "visog-job-portal-ui";
+		return "master/employment_Type";
+	}
+
+	@RequestMapping(value = "/employertype", method = RequestMethod.GET)
+	public String employertype() {
+		String message = "visog-job-portal-ui";
+		return "master/employer_Type";
+	}
+
+	@RequestMapping(value = "/filetype", method = RequestMethod.GET)
+	public String filetype() {
+		String message = "visog-job-portal-ui";
+		return "master/file_Type";
+	}
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index() {
+		String message = "visog-job-portal-ui";
+		return "main/index";
+	}
+
 }
